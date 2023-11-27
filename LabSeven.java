46f088180c6e28f3aa251e079cc1bc01a261a45d
Rpearsall267 <150750282+Rpearsall267@users.noreply.github.com>
2023-11-27 13:51:13 -0800

    Add files via upload

---
+++ LabSeven.java
@@ -0,0 +1,53 @@
+// Ron Pearsall Lab 7
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
+
+public class LabSeven{
+   /*Main method that reads file, splits file into arrays, calculates total 
+   *price, and writes prices to new file.
+   *@params: String args
+   *@returns: None void
+   */
+   public static void main(String[] args){
+      
+      String fileIn;
+      String[] orderId = new String[2820];
+      String[] productId = new String[2820];
+      double[] unitPrice = new double[2820];
+      double[] discount = new double[2820];
+      int[] quanity = new int[2820];
+      int i = 0;   
+      //reading file in, and storing info in different arrays.
+      try{
+         File readFile = new File("OrderDetails.txt");
+         Scanner in = new Scanner(readFile);
+         while (in.hasNext()){
+            fileIn = in.nextLine();
+            String[] splitString = fileIn.split(",");
+            quanity[i] = Integer.parseInt(splitString[0]);
+            unitPrice[i] = Double.parseDouble(splitString[1]);
+            orderId[i] = splitString[2];
+            productId[i] = splitString[3];
+            discount[i] = Double.parseDouble(splitString[4]);
+            i++;
+         }
+         in.close();
+      } 
+      catch (Exception e){
+           System.out.println(e.getMessage());   
+      }
+      //for loop that calculates total price and writes price to new file.
+      try{
+         PrintWriter outfile = new PrintWriter("Prices.txt");
+         for(int j = 0; j < 2820; j++){
+            double prices = unitPrice[j] * quanity[j] - (discount[j] * quanity[j] * unitPrice[j]);
+            outfile.printf("Order Id %s, Total Price $%.2f\n", orderId[j],prices);
+         }
+         outfile.close();
+      }
+      catch (Exception e){
+            System.out.println(e.getMessage());
+      }
+   }
+}
