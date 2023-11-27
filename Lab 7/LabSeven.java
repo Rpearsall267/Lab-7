abea11e811326b49f839317336c4096728019bc4
Rpearsall267 <150750282+Rpearsall267@users.noreply.github.com>
2023-11-27 13:25:56 -0800

    Add files via upload

--- Lab 7/LabSeven.java
+++ Lab 7/LabSeven.java
@@ -1,8 +1,51 @@
 // Ron Pearsall Lab 7
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
 
 public class LabSeven{
-
-
-
-
+   
+   public static void main(String[] args){
+      
+      String fileIn;
+      String[] orderId = new String[2820];
+      String[] productId = new String[2820];
+      double[] unitPrice = new double[2820];
+      double[] discount = new double[2820];
+      int[] quanity = new int[2820];
+      int i = 0;   
+      
+      try{
+         File readFile = new File("OrderDetails.txt");
+         Scanner in = new Scanner(readFile);
+         while (in.hasNext()){
+            fileIn = in.nextLine();
+            String[] splitString = fileIn.split(",");
+            quanity[i] = Integer.parseInt(splitString[0]);
+            unitPrice[i] = Double.parseDouble(splitString[1]);
+            orderId[i] = splitString[2];
+            productId[i] = splitString[3];
+            discount[i] = Double.parseDouble(splitString[4]);
+            i++;
+         }
+         in.close();
+      } 
+      catch (Exception e){
+           System.out.println(e.getMessage());   
+      }
+      try{
+         PrintWriter outfile = new PrintWriter("Prices.txt");
+         for(int j = 0; j < 2820; j++){
+            double prices = unitPrice[j] * quanity[j] - (discount[j] * quanity[j] * unitPrice[j]);
+            outfile.println(orderId[j]+", "+prices);
+         }
+         outfile.close();
+         }
+         catch (Exception e){
+            System.out.println(e.getMessage());
+         }
+   
+   
+   
+   }
 }
